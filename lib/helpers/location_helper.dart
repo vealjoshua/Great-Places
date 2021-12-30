3aede50b64bd3929532844dff4fbee72dc0cde8c
Joshua Veal-Briscoe <josh@MacBook-Pro.attlocal.net>
2021-12-30 11:34:23 -0600

    remove prints

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -9,13 +9,10 @@ class LocationHelper {
     double latitude,
     double longitude,
   }) {
-    print(GOOGLE_API_KEY);
-
     return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
   }
 
   static Future<String> getPlaceAddress(double lat, double lng) async {
-    print(GOOGLE_API_KEY);
     final url = Uri.parse(
         'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$GOOGLE_API_KEY');
     final response = await http.get(url);
